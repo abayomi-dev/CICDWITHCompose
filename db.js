8ff018963acf53aa49b7117090426dc5dfa6f426
osiephri <osiephriobukohwo@gmail.com>
2022-09-14 18:05:50 +0100

    Final stable version

--- db.js
+++ db.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
-const {
-  MONGO_USERNAME,
-  MONGO_PASSWORD,
-  MONGO_HOSTNAME,
-  MONGO_PORT,
-  MONGO_DB
-} = process.env;
+const MONGO_USERNAME = 'sammy';
+const MONGO_PASSWORD = 'your_password';
+const MONGO_HOSTNAME = '127.0.0.1';
+const MONGO_PORT = '27017';
+const MONGO_DB = 'sharkinfo';
 
 const options = {
   useNewUrlParser: true,
