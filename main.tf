413b6aecd505ed48ead48ca5a0217ff1da18e4f8
osiephri <osiephriobukohwo@gmail.com>
2022-09-15 05:00:47 +0100

    Latest commit

--- main.tf
+++ main.tf
@@ -1,15 +1,7 @@
-terraform{
-    required_providers {
-      aws{
-        source = "hashicorp/aws"
-        version = "~>3.73"
-      }
-    }
+provider "aws" {
+  region = us-east-1
 }
 
-provider "aws"{
-    region = "us-east-1"
-}
 resource aws_instance jenkins{
     count = 1
     ami = "ami-052efd3df9dad4825"
